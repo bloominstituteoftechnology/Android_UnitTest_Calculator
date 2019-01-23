daa4daefa346ff0bfe49214bb705fb6bd900b2b6
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-23 11:57:55 -0800

    Added Calculator class.

---
+++ app/src/main/java/com/joshuahalvorson/android_unittest_calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.joshuahalvorson.android_unittest_calculator;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public String addSymbol(String symbol){
+        return symbol;
+    }
+}
