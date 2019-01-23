50c62005b57c144c3566f5f1c172778c62767e14
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-23 13:43:11 -0600

    built a calculator class

---
+++ app/src/main/java/com/example/calculatorunittest/classes/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.calculatorunittest.classes;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public String addSymbol(String string){
+        return string;
+    }
+}
