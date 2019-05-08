0e05ef1242fd106098e50ea338403f94e46d9d5d
heiligbasil <basilief@yahoo.com>
2019-05-08 16:37:29 -0700

    Added unimplemented Calculator methods and wrote unit tests for them

--- app/src/main/java/com/lambdaschool/android_unittest_calculator/Calculator.java
+++ app/src/main/java/com/lambdaschool/android_unittest_calculator/Calculator.java
@@ -5,12 +5,64 @@ public class Calculator {
     private String expression;
 
     public Calculator() {
-        expression = "";
+        this.expression = "";
     }
 
     public String addSymbol(String symbol) {
         String firstChar = symbol.substring(0, 1);
         this.expression += firstChar;
         return firstChar;
+    }
+
+    public String addDecimal() {
+        return expression;
+    }
+
+    public String backspace() {
+        return expression;
+    }
+
+    public String addition() {
+        return expression;
+    }
+
+    public String subtraction() {
+        return expression;
+    }
+
+    public String multiplication() {
+        return expression;
+    }
+
+    public String division() {
+        return expression;
+    }
+
+    public String square() {
+        return expression;
+    }
+
+    public String squareRoot() {
+        return expression;
+    }
+
+    public String pi() {
+        return expression;
+    }
+
+    public String calculate() {
+        return expression;
+    }
+
+    public void reset() {
+
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
     }
 }
--- app/src/test/java/com/lambdaschool/android_unittest_calculator/CalculatorTest.java
+++ app/src/test/java/com/lambdaschool/android_unittest_calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package com.lambdaschool.android_unittest_calculator;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
@@ -15,12 +16,208 @@ public class CalculatorTest {
     }
 
     @Test
-    public void addSymbol_OneCharacter_ReturnsTrue() {
-        assertEquals("1", calculator.addSymbol("1"));
+    public void addSymbol_One() {
+        calculator.setExpression("");
+        String testSymbolToAdd = "1";
+        assertEquals(testSymbolToAdd, calculator.addSymbol(testSymbolToAdd));
     }
 
     @Test
-    public void addSymbol_MultipleCharacters_ReturnsFalse() {
-        assertEquals("1", calculator.addSymbol("123"));
+    public void addSymbol_Multiple() {
+        calculator.setExpression("123");
+        String testSymbolToAdd = "4";
+        assertEquals(calculator.getExpression() + testSymbolToAdd, calculator.addSymbol(testSymbolToAdd));
+    }
+
+    @Test
+    public void addDecimal_One() {
+        calculator.setExpression("");
+        assertEquals(".", calculator.addDecimal());
+    }
+
+    @Test
+    public void addDecimal_Multiple() {
+        calculator.setExpression("123.4");
+        assertEquals(calculator.getExpression(), calculator.addDecimal());
+    }
+
+    @Test
+    public void backspace_None() {
+        calculator.setExpression("");
+        assertEquals(calculator.getExpression(), calculator.backspace());
+    }
+
+    @Test
+    public void backspace_One() {
+        calculator.setExpression("1");
+        assertEquals(calculator.getExpression(), calculator.backspace());
+    }
+
+    @Test
+    public void backspace_Multiple() {
+        calculator.setExpression("123.4");
+        assertEquals(calculator.getExpression().substring(0, calculator.getExpression().length() - 1), calculator.backspace());
+    }
+
+    @Test
+    public void addition_First() {
+        calculator.setExpression("");
+        assertEquals(calculator.getExpression(), calculator.addition());
+    }
+
+    @Test
+    public void addition_One() {
+        calculator.setExpression("123");
+        assertEquals(calculator.getExpression() + "+", calculator.addition());
+    }
+
+    @Test
+    public void addition_Multiple() {
+        calculator.setExpression("123+");
+        assertEquals(calculator.getExpression(), calculator.addition());
+    }
+
+    @Test
+    public void subtraction_First() {
+        calculator.setExpression("");
+        assertEquals(calculator.getExpression(), calculator.subtraction());
+    }
+
+    @Test
+    public void subtraction_One() {
+        calculator.setExpression("123");
+        assertEquals(calculator.getExpression() + "-", calculator.subtraction());
+    }
+
+    @Test
+    public void subtraction_Multiple() {
+        calculator.setExpression("123-");
+        assertEquals(calculator.getExpression(), calculator.subtraction());
+    }
+
+    @Test
+    public void multiplication_First() {
+        calculator.setExpression("");
+        assertEquals(calculator.getExpression(), calculator.multiplication());
+    }
+
+    @Test
+    public void multiplication_One() {
+        calculator.setExpression("123");
+        assertEquals(calculator.getExpression() + "x", calculator.multiplication());
+    }
+
+    @Test
+    public void multiplication_Multiple() {
+        calculator.setExpression("123x");
+        assertEquals(calculator.getExpression(), calculator.multiplication());
+    }
+
+    @Test
+    public void division_First() {
+        calculator.setExpression("");
+        assertEquals(calculator.getExpression(), calculator.division());
+    }
+
+    @Test
+    public void division_One() {
+        calculator.setExpression("123");
+        assertEquals(calculator.getExpression() + "÷", calculator.division());
+    }
+
+    @Test
+    public void division_Multiple() {
+        calculator.setExpression("123÷");
+        assertEquals(calculator.getExpression(), calculator.division());
+    }
+
+    @Test
+    public void square_First() {
+        calculator.setExpression("");
+        assertEquals(calculator.getExpression(), calculator.square());
+    }
+
+    @Test
+    public void square_One() {
+        calculator.setExpression("123");
+        assertEquals(calculator.getExpression() + "²", calculator.square());
+    }
+
+    @Test
+    public void square_Multiple() {
+        calculator.setExpression("123²");
+        assertEquals(calculator.getExpression(), calculator.square());
+    }
+
+    @Test
+    public void squareRoot_First() {
+        calculator.setExpression("");
+        assertEquals(calculator.getExpression(), calculator.squareRoot());
+    }
+
+    @Test
+    public void squareRoot_One() {
+        calculator.setExpression("123");
+        assertEquals(calculator.getExpression() + "√", calculator.squareRoot());
+    }
+
+    @Test
+    public void squareRoot_Multiple() {
+        calculator.setExpression("123√");
+        assertEquals(calculator.getExpression(), calculator.squareRoot());
+    }
+
+    @Test
+    public void pi_One() {
+        calculator.setExpression("");
+        assertEquals(calculator.getExpression() + "π", calculator.pi());
+    }
+
+    @Test
+    public void pi_Multiple() {
+        calculator.setExpression("π");
+        assertEquals(calculator.getExpression(), calculator.pi());
+    }
+
+    @Test
+    public void calculate_Blank() {
+        calculator.setExpression("");
+        assertEquals(calculator.getExpression(), calculator.calculate());
+    }
+
+    @Test
+    public void calculate_OneOperand() {
+        calculator.setExpression("123");
+        assertEquals(calculator.getExpression(), calculator.calculate());
+    }
+
+    @Test
+    public void calculate_Addition() {
+        calculator.setExpression("123+123");
+        assertEquals("246", calculator.calculate());
+    }
+
+    @Test
+    public void calculate_Subtraction() {
+        calculator.setExpression("123-123");
+        assertEquals("0", calculator.calculate());
+    }
+
+    @Test
+    public void calculate_Multiplication() {
+        calculator.setExpression("123x123");
+        assertEquals("15129", calculator.calculate());
+    }
+
+    @Test
+    public void calculate_Division() {
+        calculator.setExpression("123÷123");
+        assertEquals("1", calculator.calculate());
+    }
+
+    @Test
+    public void reset() {
+        calculator.reset();
+        assertThat(calculator.getExpression(), equalTo(""));
     }
 }
