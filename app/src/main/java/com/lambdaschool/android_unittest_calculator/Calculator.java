fbda2e8942e44348a51426871ea5fbf47ef314af
heiligbasil <basilief@yahoo.com>
2019-05-08 14:26:04 -0700

    Added the Calculator class data member

--- app/src/main/java/com/lambdaschool/android_unittest_calculator/Calculator.java
+++ app/src/main/java/com/lambdaschool/android_unittest_calculator/Calculator.java
@@ -2,10 +2,15 @@ package com.lambdaschool.android_unittest_calculator;
 
 public class Calculator {
 
-    public Calculator() { }
+    private String expression;
+
+    public Calculator() {
+        expression = "";
+    }
 
     public String addSymbol(String symbol) {
         String firstChar = symbol.substring(0, 1);
+        this.expression += firstChar;
         return firstChar;
     }
 }
