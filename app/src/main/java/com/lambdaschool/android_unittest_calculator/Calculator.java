24c6cdf4204cc78c82fd8189f738dbd2f8c670fc
heiligbasil <basilief@yahoo.com>
2019-05-08 14:20:35 -0700

    Switched class and tests from static to instantiation

--- app/src/main/java/com/lambdaschool/android_unittest_calculator/Calculator.java
+++ app/src/main/java/com/lambdaschool/android_unittest_calculator/Calculator.java
@@ -2,10 +2,9 @@ package com.lambdaschool.android_unittest_calculator;
 
 public class Calculator {
 
-    public Calculator() {
-    }
+    public Calculator() { }
 
-    public static String addSymbol(String symbol) {
+    public String addSymbol(String symbol) {
         String firstChar = symbol.substring(0, 1);
         return firstChar;
     }
--- app/src/test/java/com/lambdaschool/android_unittest_calculator/CalculatorTest.java
+++ app/src/test/java/com/lambdaschool/android_unittest_calculator/CalculatorTest.java
@@ -1,17 +1,26 @@
 package com.lambdaschool.android_unittest_calculator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void initialize() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void addSymbol_OneCharacter_ReturnsTrue() {
-        assertEquals("1", Calculator.addSymbol("1"));
+        assertEquals("1", calculator.addSymbol("1"));
     }
 
     @Test
     public void addSymbol_MultipleCharacters_ReturnsFalse() {
-        assertEquals("1", Calculator.addSymbol("123"));
+        assertEquals("1", calculator.addSymbol("123"));
     }
 }
