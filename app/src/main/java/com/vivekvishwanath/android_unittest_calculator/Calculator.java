8dc57e53bbd409c6d0218c2c22417111a1bfe927
VivekV95 <vivekv210@gmail.com>
2019-05-08 17:45:32 -0500

    Added and tested all basic operations

--- app/src/main/java/com/vivekvishwanath/android_unittest_calculator/Calculator.java
+++ app/src/main/java/com/vivekvishwanath/android_unittest_calculator/Calculator.java
@@ -20,8 +20,6 @@ public class Calculator {
                 enteredSymbols += symbol;
                 return enteredSymbols;
             } else {
-                enteredSymbols = enteredSymbols.replace(".", "");
-                enteredSymbols += symbol;
                 return enteredSymbols;
             }
         } else {
@@ -37,6 +35,41 @@ public class Calculator {
 
         double sum = before + after;
         enteredSymbols = Double.toString(sum);
+        return enteredSymbols;
+    }
+
+    public String subtract() {
+        String[] numbers = enteredSymbols.split("-");
+        double before = Double.parseDouble(numbers[0]);
+        double after = Double.parseDouble(numbers[1]);
+
+        double difference = before - after;
+        enteredSymbols = Double.toString(difference);
+        return enteredSymbols;
+    }
+
+    public String multiply() {
+        String[] numbers = enteredSymbols.split("\\*");
+        double before = Double.parseDouble(numbers[0]);
+        double after = Double.parseDouble(numbers[1]);
+
+        double product = before * after;
+        enteredSymbols = Double.toString(product);
+        return enteredSymbols;
+    }
+
+    public String divide() {
+        String[] numbers = enteredSymbols.split("/");
+        double before = Double.parseDouble(numbers[0]);
+        double after = Double.parseDouble(numbers[1]);
+
+        double quotient = before / after;
+        enteredSymbols = Double.toString(quotient);
+        return enteredSymbols;
+    }
+
+    public String clear() {
+        enteredSymbols = "";
         return enteredSymbols;
     }
 
--- app/src/test/java/com/vivekvishwanath/android_unittest_calculator/CalculatorTest.java
+++ app/src/test/java/com/vivekvishwanath/android_unittest_calculator/CalculatorTest.java
@@ -9,12 +9,14 @@ public class CalculatorTest {
 
     @Test
     public void calculatorTest_AddSingleCharacter_WhenNothingElseAdded() {
+        calculator.clear();
         String singleCharacter = "1";
         assertEquals(singleCharacter, calculator.addSymbol(singleCharacter));
     }
 
     @Test
     public void calculatorTest_AddSingleCharacter_WhenOtherCharacterAdded() {
+        calculator.clear();
         String previousCharacter = "1";
         String newCharacter = "2";
         String combinedCharacters = previousCharacter + newCharacter;
@@ -24,6 +26,7 @@ public class CalculatorTest {
 
     @Test
     public void calculatorTest_AddDecimal_WithoutPreviousDecimalAdded() {
+        calculator.clear();
         String previousCharacter = "1";
         String newCharacter = ".";
         String combinedCharacters = previousCharacter + newCharacter;
@@ -33,6 +36,7 @@ public class CalculatorTest {
 
     @Test
     public void calculatorTest_AddDecimal_WithPreviousDecimalAdded() {
+        calculator.clear();
         String previousCharacter = "1.1";
         String newCharacter = ".";
         String combinedCharacters = "11.";
@@ -42,9 +46,37 @@ public class CalculatorTest {
 
     @Test
     public void calculatorTest_AddTwoNumbers_WithoutPreviousOperations() {
+        calculator.clear();
         String entry = "1+2";
-        String sum = "3";
+        String sum = "3.0";
         calculator.addSymbol(entry);
         assertEquals(sum, calculator.add());
     }
+
+    @Test
+    public void calculatorTest_SubtractTwoNumbers_WithoutPreviousOperations() {
+        calculator.clear();
+        String entry = "2-1";
+        String difference = "1.0";
+        calculator.addSymbol(entry);
+        assertEquals(difference, calculator.subtract());
+    }
+
+    @Test
+    public void calculatorTest_MultiplyTwoNumbers_WithoutPreviousOperations() {
+        String entry = "1*2";
+        String product = "2.0";
+        calculator.addSymbol(entry);
+        assertEquals(product, calculator.multiply());
+    }
+
+    @Test
+    public void calculatorTest_DivideTwoNumbers_WithoutPreviousOperations() {
+        String entry = "10/2";
+        String quotient = "5.0";
+        calculator.addSymbol(entry);
+        assertEquals(quotient, calculator.divide());
+    }
+
+
 }
