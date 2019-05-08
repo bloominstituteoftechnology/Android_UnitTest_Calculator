b9e5b01723f2f218060e5e294896dbf8a37d6c9c
Jake Esveld <thejakeesveld@gmail.com>
2019-05-08 11:17:10 -0700

    Add functionality for multiple characters

--- app/src/main/java/com/jakeesveld/calculator/Objects/Calculator.java
+++ app/src/main/java/com/jakeesveld/calculator/Objects/Calculator.java
@@ -1,10 +1,15 @@
 package com.jakeesveld.calculator.Objects;
 
 public class Calculator {
+    private String equation;
     public Calculator() {
+        equation = "";
     }
 
-    public static String addSymbol(String symbol){
-        return symbol;
+    public String addSymbol(String symbol){
+        StringBuilder builder = new StringBuilder();
+        builder.append(equation).append(symbol);
+        equation = builder.toString();
+        return equation;
     }
 }
--- app/src/test/java/com/jakeesveld/calculator/Objects/CalculatorTest.java
+++ app/src/test/java/com/jakeesveld/calculator/Objects/CalculatorTest.java
@@ -1,21 +1,29 @@
 package com.jakeesveld.calculator.Objects;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+    Calculator calculator;
+
+    @Before
+    public void setUp(){
+        calculator = new Calculator();
+    }
 
     @Test
     public void addSymbolSingleCharacter() {
-        assertThat(Calculator.addSymbol("d"), equalTo("d"));
+        assertThat(calculator.addSymbol("d"), equalTo("d"));
     }
 
     @Test
     public void addSymbolMultipleCharacters() {
-        String returnedString = Calculator.addSymbol("d");
-        returnedString = Calculator.addSymbol("d");
+        String returnedString;
+        calculator.addSymbol("d");
+        returnedString = calculator.addSymbol("d");
         assertThat(returnedString, equalTo("dd"));
     }
 
