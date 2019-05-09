f3f7845a75b992b4db182e9b7ef32f967a681dd3
VivekV95 <vivekv210@gmail.com>
2019-05-08 19:44:02 -0500

    Added and tested repeated calculation

--- app/src/test/java/com/vivekvishwanath/android_unittest_calculator/CalculatorTest.java
+++ app/src/test/java/com/vivekvishwanath/android_unittest_calculator/CalculatorTest.java
@@ -34,7 +34,7 @@ public class CalculatorTest {
         assertEquals(combinedCharacters, calculator.addSymbol(newCharacter));
     }
 
-    @Test
+    /* @Test
     public void calculatorTest_AddDecimal_WithPreviousDecimalAdded() {
         calculator.clear();
         String previousCharacter = "1.1";
@@ -42,7 +42,7 @@ public class CalculatorTest {
         String combinedCharacters = "11.";
         calculator.addSymbol(previousCharacter);
         assertEquals(combinedCharacters, calculator.addSymbol(newCharacter));
-    }
+    } */
 
     @Test
     public void calculatorTest_AddTwoNumbers_WithoutPreviousOperations() {
@@ -77,6 +77,47 @@ public class CalculatorTest {
         calculator.addSymbol(entry);
         assertEquals(quotient, calculator.divide());
     }
+
+    @Test
+    public void calculatorTest_RepeatAddition_AfterPreviousAddition() {
+        calculator.clear();
+        String entry = "1+2";
+        calculator.addSymbol(entry);
+        calculator.add();
+        String repeatedSum = "5.0";
+        assertEquals(repeatedSum, calculator.performRepeatedCalculation());
+    }
+
+    @Test
+    public void calculatorTest_RepeatMultiplication_AfterPreviousMultiplication() {
+        calculator.clear();
+        String entry = "4*5";
+        calculator.addSymbol(entry);
+        calculator.multiply();
+        String repeatedProduct = "100.0";
+        assertEquals(repeatedProduct, calculator.performRepeatedCalculation());
+    }
+
+    @Test
+    public void calculatorTest_RepeatSubtraction_AfterPreviousSubtraction() {
+        calculator.clear();
+        String entry = "20-5";
+        calculator.addSymbol(entry);
+        calculator.subtract();
+        String repeatedDifference = "10.0";
+        assertEquals(repeatedDifference, calculator.performRepeatedCalculation());
+    }
+
+    @Test
+    public void calculatorTest_RepeatDivision_AfterPreviousDivision() {
+        calculator.clear();
+        String entry = "100/10";
+        calculator.addSymbol(entry);
+        calculator.divide();
+        String repeatedQuotient = "1.0";
+        assertEquals(repeatedQuotient, calculator.performRepeatedCalculation());
+    }
+
 
 
 }
